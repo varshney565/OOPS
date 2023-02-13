5a5031b020ecadffd8f62d8924b0409c8a7dbecc
Shivam Varshney <uietcse1923shivamvarshney@kuk.ac.in>
2023-02-13 23:42:42 +0530

    StaticMembers

--- OOPS1and2/AdvanceOOPS/StaticMembers/student.cpp
+++ OOPS1and2/AdvanceOOPS/StaticMembers/student.cpp
@@ -3,6 +3,11 @@ class Student{
     int age;
     int rool;
     static int totalStudents;
+    Student(int age,int rool){
+        this->age = age;
+        this->rool = rool;
+        totalStudents++;
+    }
 };
 
 int Student::totalStudents = 0; //initialization of static fields
