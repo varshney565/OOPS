163343b10e2466acf60b5726de858d155f72605b
Shivam Varshney <uietcse1923shivamvarshney@kuk.ac.in>
2023-02-13 21:29:54 +0530

    GetterAndSetter

--- OOPS1/Lecture1&2/StudentUse.cpp
+++ OOPS1/Lecture1&2/StudentUse.cpp
@@ -5,9 +5,12 @@ int main(){
     //creating statically.
     Student s1,s2;
     s1.display();
+
+    
     //creating dynamically.
     Student *s3 = new Student;
-    s3->age = 22;
+    // s3->age = 22; //this line will give error
+    s3->setAge(22);
     s3->display();
     return 0;
 }
--- OOPS1/Lecture1&2/student.cpp
+++ OOPS1/Lecture1&2/student.cpp
@@ -1,10 +1,19 @@
 class Student{
     public : 
         int RollNo;
+    private :
         int age;
-
+    public :
         void display(){
             cout<<"age"<<" : "<<age<<"\n";
             cout<<"roll_number : "<<RollNo<<"\n";
+        }
+
+        int getAge(){
+            return this->age;
+        }
+
+        void setAge(int age){
+            this->age = age;
         }
 };
