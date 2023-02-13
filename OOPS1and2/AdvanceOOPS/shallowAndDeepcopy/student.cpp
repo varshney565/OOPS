ea61dfb2712a6244e493bf1f34d6e51379094d64
Shivam Varshney <uietcse1923shivamvarshney@kuk.ac.in>
2023-02-13 22:34:57 +0530

    ShallowAndDeepCopy

---
+++ OOPS1and2/AdvanceOOPS/shallowAndDeepcopy/student.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+class Student {
+    char *name;
+    public : 
+
+    Student(char*name){
+        //shallow copy
+        this->name = name;
+
+        //deep copy
+        this->name = new char[strlen(name)+1];
+        strcpy(name,this->name);
+    }
+};
