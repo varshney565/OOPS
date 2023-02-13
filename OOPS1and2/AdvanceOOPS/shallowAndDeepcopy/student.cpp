c26f1118bf3232f83fe81751f3b4ea790af649a0
Shivam Varshney <uietcse1923shivamvarshney@kuk.ac.in>
2023-02-13 23:08:03 +0530

    WritingOwnCopyConstructor

--- OOPS1and2/AdvanceOOPS/shallowAndDeepcopy/student.cpp
+++ OOPS1and2/AdvanceOOPS/shallowAndDeepcopy/student.cpp
@@ -13,4 +13,10 @@ class Student {
         this->name = new char[strlen(name)+1];
         strcpy(name,this->name);
     }
+
+    //writing our own copy constructor
+    Student(const Student &s){
+        this->name = new char[strlen(name)+1];
+        strcpy(name,this->name);
+    }
 };
