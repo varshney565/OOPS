2c934a0b9bfe53e95202762dca2ca2ea76537919
Shivam Varshney <uietcse1923shivamvarshney@kuk.ac.in>
2023-02-13 21:37:23 +0530

    Constructor

--- OOPS1/Lecture1&2/StudentUse.cpp
+++ OOPS1/Lecture1&2/StudentUse.cpp
@@ -6,9 +6,9 @@ int main(){
     Student s1,s2;
     s1.display();
 
-    
+
     //creating dynamically.
-    Student *s3 = new Student;
+    Student *s3 = new Student(12,101);
     // s3->age = 22; //this line will give error
     s3->setAge(22);
     s3->display();
--- OOPS1/Lecture1&2/student.cpp
+++ OOPS1/Lecture1&2/student.cpp
@@ -4,6 +4,17 @@ class Student{
     private :
         int age;
     public :
+        //default contructor
+        Student(){
+
+        }
+
+        //Parameterized constructor
+        Student(int age,int RollNo){
+            this->age = age;
+            this->RollNo = RollNo;
+        }
+        
         void display(){
             cout<<"age"<<" : "<<age<<"\n";
             cout<<"roll_number : "<<RollNo<<"\n";
