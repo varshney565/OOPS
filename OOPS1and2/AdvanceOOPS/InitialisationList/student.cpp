8a92387b21dd32e7b97fe9ab80aca7c2d9580444
shivam <uietcse1923shivamvarshney@kuk.ac.in>
2024-03-17 13:27:30 +0530

    hii

--- OOPS1and2/AdvanceOOPS/InitialisationList/student.cpp
+++
@@ -1,9 +0,0 @@
-class Student{
-    public : 
-    const int a;
-    int &b;
-    int c;
-    Student(int a):a(a),b(this->c){ //Initialisation list
-        //constructor
-    }
-};
--- OOPS1and2/BasicsOfClassAndObject/product.cpp
+++
@@ -1,6 +0,0 @@
-class Product{
-    public :
-        int price;
-        int id;
-        char name[100];
-};
