e9aa44954acecb32766659f7e6e1c3190ecc2f49
shivam <uietcse1923shivamvarshney@kuk.ac.in>
2024-03-28 00:34:53 +0530

    arrays

---
+++ java_lld_and_oops/video6/Array/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String [] args) {
+        int arr[] = new int[5];
+        //int [] arr = new int[5]
+        //Both are correct.
+        arr[0] = 1;arr[1] = 2;
+
+        int [][]brr = new int[5][4];
+    } 
+}
