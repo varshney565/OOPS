10d0b2d42d61b70466e1f837f9a1791d1ebfc816
shivamo7 <shivam.varshney@antiersolutions.com>
2024-04-28 13:37:37 +0530

    concrete_class

---
+++ java_lld_and_oops/video10/concrete_class/Person.java
@@ -0,0 +1,13 @@
+package video10.concrete_class;
+
+public class Person {
+    int empId;
+
+    Person(int empId) {
+        this.empId = empId;
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+}
